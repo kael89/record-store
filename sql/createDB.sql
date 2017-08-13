d003695b09b4bfcce35abfbf9eade2c853e1a842
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2017-08-13 22:54:43 +1000

    Update DB with tables regarding artists

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -4,11 +4,89 @@ USE record_store;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
-    userID SMALLINT NOT NULL AUTO_INCREMENT,
+    userID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(64) NOT NULL,
     admin BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY(userID)
+);
+
+DROP TABLE IF EXISTS artists;
+CREATE TABLE artists(
+    artistID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(2),
+    foundationYear YEAR,
+    logo VARCHAR(255),
+    photo VARCHAR(255),
+    PRIMARY KEY(artistID)
+);
+
+DROP TABLE IF EXISTS labels;
+CREATE TABLE labels(
+    labelID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(2),
+    foundationYear YEAR,
+    logo VARCHAR(255),
+    PRIMARY KEY(labelID)
+);
+
+DROP TABLE IF EXISTS records;
+CREATE TABLE records(
+    recordID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    labelID SMALLINT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    releaseDate DATE,
+    cover VARCHAR(255),
+    price FLOAT(5,2),
+    PRIMARY KEY(recordID),
+    FOREIGN KEY(labelID) REFERENCES labels(labelID)
+);
+
+DROP TABLE IF EXISTS tracks;
+CREATE TABLE tracks(
+    trackID MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artistID SMALLINT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    duration SMALLINT UNSIGNED,
+    PRIMARY KEY(trackID),
+    FOREIGN KEY (artistID) REFERENCES artists(artistID)
+);
+
+DROP TABLE IF EXISTS genres;
+CREATE TABLE genres(
+    genreID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(genreID)
+);
+
+DROP TABLE IF EXISTS artistsRecords;
+CREATE TABLE artistsRecords(
+    artistID SMALLINT UNSIGNED NOT NULL,
+    recordID SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (artistID, recordID),
+    FOREIGN KEY (artistID) REFERENCES artists(artistID),
+    FOREIGN KEY (recordID) REFERENCES records(recordID)
+);
+
+DROP TABLE IF EXISTS recordsTracks;
+CREATE TABLE recordsTracks(
+    recordID SMALLINT UNSIGNED NOT NULL,
+    trackID MEDIUMINT UNSIGNED NOT NULL,
+    trackNumber TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (recordID, trackID),
+    FOREIGN KEY (recordID) REFERENCES records(recordID),
+    FOREIGN KEY (trackID) REFERENCES tracks(trackID)
+);
+
+DROP TABLE IF EXISTS recordsGenres;
+CREATE TABLE recordsGenres(
+    recordID SMALLINT UNSIGNED NOT NULL,
+    genreID SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (recordID, genreID),
+    FOREIGN KEY (recordID) REFERENCES records(recordID),
+    FOREIGN KEY (genreID) REFERENCES genres(genreID)
 );
--- sql/populateDB.sql
+++ sql/populateDB.sql
@@ -1,8 +1,137 @@
 USE record_store;
 
-#users(userId,firstName,lastName,email,password,admin)
+
+##users(userId,firstName,lastName,email,password,admin)
 DELETE FROM users;
 INSERT INTO users VALUES(1,'Kostas','Karvounis','kos.karvounis.zoumpos@gmail.com','11875FF7B881A3BFE8550E09ACD28F8E57DC24933C0B4053300E588E9B97D6B1',TRUE);
 INSERT INTO users VALUES(2,'Kostas','Karvounis','kael1989@gmail.com','11875FF7B881A3BFE8550E09ACD28F8E57DC24933C0B4053300E588E9B97D6B1',TRUE);
 #INSERT INTO users VALUES(3,'Elena','Kyriaki','elena.kyriaki@gmail.com','2E8F5D4AA19B860AEB84E2A6C45713D81F009FD2E691356B17FF76FBABDF47B2',FALSE);
 #INSERT INTO users VALUES(4,'Alexis','Karvounis','al3xkarv@gmail.com','EC03ACE04805C0B06CE7A3E7D6882B9311714CC03C342EA60B6A30A411994313',FALSE);
+
+
+##artists(artistID, name, country, foundationDate, logo, photo)
+DELETE FROM artists;
+INSERT INTO artists VALUES(1, 'Metallica', 'US', 1981, NULL, NULL);
+INSERT INTO artists VALUES(2, 'Iron Maiden', 'GB', 1975, NULL, NULL);
+INSERT INTO artists VALUES(3, 'Black Sabbath', 'GB', 1968, NULL, NULL);
+INSERT INTO artists VALUES(4, 'Judas Priest', 'GB', 1969, NULL, NULL);
+INSERT INTO artists VALUES(5, 'Sepultura', 'BR', 1984, NULL, NULL);
+
+
+##labels(labelID, name, country, foundationYear, logo)
+DELETE FROM labels;
+INSERT INTO labels VALUES(1, 'Megaforce', 'US', 1982, NULL);
+INSERT INTO labels VALUES(2, 'EMI', 'UK', 1931, NULL);
+INSERT INTO labels VALUES(3, 'Vertigo', 'UK', 1969, NULL);
+INSERT INTO labels VALUES(4, 'Gull', 'UK', 1974, NULL);
+INSERT INTO labels VALUES(5, 'Roadrunner', 'NL', 1980, NULL);
+INSERT INTO labels VALUES(6, 'Columbia', 'US', 1987, NULL);
+
+
+##records(recordID, labelID, title, releaseDate, cover, price)
+DELETE FROM records;
+INSERT INTO records VALUES(1, 1, 'Kill ''em All', '1983-07-25', NULL, 09.99);
+INSERT INTO records VALUES(2, 1, 'Ride The Lightning', '1984-07-27', NULL, 09.75);
+INSERT INTO records VALUES(3, 2, 'The Number of the Beast', '1982-03-22', NULL, 12.15);
+INSERT INTO records VALUES(4, 3, 'Black Sabbath', '1970-02-13', NULL, 08.99);
+INSERT INTO records VALUES(5, 3, 'Paranoid', '1970-09-18', NULL, 08.99);
+INSERT INTO records VALUES(6, 4, 'Sad Wings of Destiny', '1976-03-23', NULL, 07.50);
+INSERT INTO records VALUES(7, 6, 'British Steel', '1980-04-14', NULL, 08.99);
+INSERT INTO records VALUES(8, 6, 'Painkiller', '1990-09-03', NULL, 12.10);
+INSERT INTO records VALUES(9, 5, 'Roots', '1996-02-20', NULL, 11.70);
+INSERT INTO records VALUES(10, 5, 'Chaos A.D.', '1993-10-19', NULL, 11.30);
+
+
+##tracks(trackID, artistID, title, duration)
+DELETE FROM tracks;
+#Metallica - Kill 'em All (10 tracks)
+INSERT INTO tracks VALUES(1, 1, 'Hit the Lights', 256);
+INSERT INTO tracks VALUES(2, 1, 'The Four Horsemen', 433);
+INSERT INTO tracks VALUES(3, 1, 'Motorbreath', 188);
+INSERT INTO tracks VALUES(4, 1, 'Jump in the Fire', 281);
+INSERT INTO tracks VALUES(5, 1, '(Anesthesia) - Pulling Teeth', 255);
+INSERT INTO tracks VALUES(6, 1, 'Whiplash', 250);
+INSERT INTO tracks VALUES(7, 1, 'Phantom Lord', 302);
+INSERT INTO tracks VALUES(8, 1, 'No Remorse', 386);
+INSERT INTO tracks VALUES(9, 1, 'Seek & Destroy', 415);
+INSERT INTO tracks VALUES(10, 1, 'Metal Militia', 309);
+#Metallica - Ride the Lightning (8 tracks)
+INSERT INTO tracks VALUES(11, 1, 'Fight Fire with Fire', 285);
+INSERT INTO tracks VALUES(12, 1, 'Ride the Lightning', 396);
+INSERT INTO tracks VALUES(13, 1, 'For Whom the Bell Tolls', 309);
+INSERT INTO tracks VALUES(14, 1, 'Fade to Black', 417);
+INSERT INTO tracks VALUES(15, 1, 'Trapped Under Ice', 244);
+INSERT INTO tracks VALUES(16, 1, 'Escape', 263);
+INSERT INTO tracks VALUES(17, 1, 'Creeping Death', 396);
+INSERT INTO tracks VALUES(18, 1, 'The Call of Ktulu', 523);
+#Iron Maiden - The Number of the Beast (9 tracks)
+INSERT INTO tracks VALUES(19, 2, 'Invaders', NULL);
+INSERT INTO tracks VALUES(20, 2, 'Children of the Damned', NULL);
+INSERT INTO tracks VALUES(21, 2, 'The Prisoner', NULL);
+INSERT INTO tracks VALUES(22, 2, '22 Acacia Avenue', NULL);
+INSERT INTO tracks VALUES(23, 2, 'The Number of the Beast', NULL);
+INSERT INTO tracks VALUES(24, 2, 'Run to the Hills', NULL);
+INSERT INTO tracks VALUES(25, 2, 'Gangland', NULL);
+INSERT INTO tracks VALUES(26, 2, 'Hallowed By Thy Name', NULL);
+
+
+##genres(genreID, name)
+DELETE FROM genres;
+INSERT INTO genres VALUES(1, 'Heavy Metal');
+INSERT INTO genres VALUES(2, 'Thrash Metal');
+INSERT INTO genres VALUES(3, 'Groove Metal');
+INSERT INTO genres VALUES(4, 'Nu Metal');
+
+
+##artistsRecords(artistID, recordID)
+DELETE FROM artistsRecords;
+INSERT INTO artistsRecords VALUES(1, 1);
+INSERT INTO artistsRecords VALUES(1, 2);
+INSERT INTO artistsRecords VALUES(2, 3);
+INSERT INTO artistsRecords VALUES(3, 4);
+INSERT INTO artistsRecords VALUES(3, 5);
+INSERT INTO artistsRecords VALUES(4, 6);
+INSERT INTO artistsRecords VALUES(4, 7);
+INSERT INTO artistsRecords VALUES(4, 8);
+INSERT INTO artistsRecords VALUES(5, 9);
+INSERT INTO artistsRecords VALUES(5, 10);
+
+
+##recordsTracks(recordId, trackID, trackNumber)
+DELETE FROM recordsTracks;
+#Metallica - Kill 'em All (10 tracks)
+INSERT INTO recordsTracks VALUES(1, 1, 1);
+INSERT INTO recordsTracks VALUES(1, 2, 2);
+INSERT INTO recordsTracks VALUES(1, 3, 3);
+INSERT INTO recordsTracks VALUES(1, 4, 4);
+INSERT INTO recordsTracks VALUES(1, 5, 5);
+INSERT INTO recordsTracks VALUES(1, 6, 6);
+INSERT INTO recordsTracks VALUES(1, 7, 7);
+INSERT INTO recordsTracks VALUES(1, 8, 8);
+INSERT INTO recordsTracks VALUES(1, 9, 9);
+INSERT INTO recordsTracks VALUES(1, 10, 10);
+#Metallica - Ride the Lightning (8 tracks)
+INSERT INTO recordsTracks VALUES(2, 11, 1);
+INSERT INTO recordsTracks VALUES(2, 12, 2);
+INSERT INTO recordsTracks VALUES(2, 13, 3);
+INSERT INTO recordsTracks VALUES(2, 14, 4);
+INSERT INTO recordsTracks VALUES(2, 15, 5);
+INSERT INTO recordsTracks VALUES(2, 16, 6);
+INSERT INTO recordsTracks VALUES(2, 17, 7);
+INSERT INTO recordsTracks VALUES(2, 18, 8);
+#Iron Maiden - The Number of the Beast (9 tracks)
+INSERT INTO recordsTracks VALUES(3, 19, 1);
+INSERT INTO recordsTracks VALUES(3, 20, 2);
+INSERT INTO recordsTracks VALUES(3, 21, 3);
+INSERT INTO recordsTracks VALUES(3, 22, 4);
+INSERT INTO recordsTracks VALUES(3, 23, 5);
+INSERT INTO recordsTracks VALUES(3, 24, 6);
+INSERT INTO recordsTracks VALUES(3, 25, 7);
+INSERT INTO recordsTracks VALUES(3, 26, 8);
+
+
+##recordsGenres(recordID, genreID)
+DELETE FROM recordsGenres;
+INSERT INTO recordsGenres VALUES(1, 2);
+INSERT INTO recordsGenres VALUES(2, 2);
+INSERT INTO recordsGenres VALUES(3, 1);
