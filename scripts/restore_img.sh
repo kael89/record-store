c8bc4bf6ea6d6b60d339537e9446bc43125fda8b
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2017-10-29 20:46:08 +1100

    Add proper permissions in restore image script

--- scripts/restore_img.sh
+++ scripts/restore_img.sh
@@ -3,10 +3,11 @@
 LOCATION="/opt/lampp/htdocs/record-store/img"
 
 cd $LOCATION
-if [ $1 = '-d' ]
+if [ $# -gt 0 ] && [ $1 == "-d" ]
 then
     echo Clearing folder...
     sudo rm -r *
 fi
 sudo cp -r .backup/* .
+sudo chmod +777 * -R
 echo Images successfully restored!
