8ea58531864cf4b57261c5b31891f57c6a3fc10c
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2017-11-09 20:05:44 +1100

    Serve jQuery and Bootstrap via CDN

--- templates/header.php
+++ templates/header.php
@@ -23,12 +23,15 @@ if ($cat) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.2">
 
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- bootstrap.css 3.3.7 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/details.css">
 
-    <script src="js/lib/jquery-3.2.1.js"></script>
-    <script src="js/lib/bootstrap.js"></script>
+    <!-- jquery.js 3.2.1 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- bootstrap.js 3.3.7 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="js/lib/library.js"></script>
     <script src="js/tracklist.js"></script>
     <script src="js/script.js"></script>
